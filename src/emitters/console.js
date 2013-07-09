f5404172f3198f85b0195ac8ebd3ac36f5bb93d8
Robert (Jamie) Munro <rjmunro@arjam.net>
2013-07-09 15:32:30 +0100

    Make console.js pass jslint

--- src/emitters/console.js
+++ src/emitters/console.js
@@ -1,22 +1,26 @@
-(function(_rex_){
-	_rex_.emitter('console', function(events){
-		for(var i in events){
-			if(events.hasOwnProperty(i)){
-				for (var j=0; j < events[i].length; j++) {
-					var time = events[i][j];
-					var seconds;
-					if(time[1]){
-						// start stop
-						seconds = time[0] / 1000;
-						var duration = (time[1] - time[0]) / 1000;
-						console.log('@' + seconds + 's ('+duration+'s): ' + i);
-					} else {
-						seconds = events[i][j] / 1000;
-						console.log('@' + seconds + 's: ' + i);
-					}
-					
-				}
-			}
-		}
-	});
+/*jslint newcap: true, nomen: true, vars: true */
+/*global _rex_ */
+(function (_rex_) {
+    "use strict";
+    _rex_.emitter('console', function (events) {
+        var i, j;
+        for (i in events) {
+            if (events.hasOwnProperty(i)) {
+                for (j = 0; j < events[i].length; j += 1) {
+                    var time = events[i][j];
+                    var seconds;
+                    if (time[1]) {
+                        // start stop
+                        seconds = time[0] / 1000;
+                        var duration = (time[1] - time[0]) / 1000;
+                        console.log('@' + seconds + 's (' + duration + 's): ' + i);
+                    } else {
+                        seconds = events[i][j] / 1000;
+                        console.log('@' + seconds + 's: ' + i);
+                    }
+
+                }
+            }
+        }
+    });
 }(_rex_));
